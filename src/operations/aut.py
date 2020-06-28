eee4763c3c4a80173808cd776b2299a321c6a703
MrCamoga <mrcamoga@gmail.com>
2020-06-28 14:16:11 +0200

    Added automorphism group (WIP)

--- src/operations/aut.py
+++ src/operations/aut.py
@@ -1,8 +1,51 @@
-from group import functioninverse
+from group import functioninverse, composition
 from groups import Group
 
-#TODO
-def Aut(G):
-    # G = Cyclic(3)
-    # Aut.inverse = lambda f: Aut.index(functioninverse(f))
-    return Group(None, None, None)
+def Aut(G, gens = None):
+    """
+        Group or automorphisms of G
+        gens: set/list with generators of G
+    """
+
+    automorphisms = [dict()]
+
+    if gens is None:
+        gens = G.generators()
+    gens = list(gens)
+    
+    ordersd = G.orders(True)
+    genorders = [G.order(g) for g in gens]
+
+    for g in range(len(gens)):
+        newaut = []
+        for a in automorphisms:
+            R = ordersd[genorders[g]] - G.subgroup(a.values())
+            for fg in R:
+                automorphism = dict(a)
+                automorphism[gens[g]] = fg
+                newaut.append(automorphism)
+                
+        del automorphisms
+        automorphisms = newaut
+
+    def e(k):
+        return automorphisms[k]
+
+    def index(e):
+        return automorphisms.index(e)
+
+    def op(a,b):
+        """
+            a*b = b◦a
+        """
+        fa = G.automorphism(automorphisms[a])
+        fb = G.automorphism(automorphisms[b])
+        fc = composition(fa,fb)
+        c = {i:fc[i] for i in gens}
+        return index(c)
+
+    Aut = Group(len(automorphisms), e, op)
+    Aut.index = index
+    Aut.inverse = lambda f: index(functioninverse(G.automorphism(Aut[f])))
+    
+    return Aut
